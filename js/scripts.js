02b178c233221be2d2ed4e8296e608549e66e81d
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2024-06-29 03:08:58 -0600

    Se agrega la estructura de carpetas

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+//Seleccionar contenido HTML
+
+// querySelector 
+const heading = document.querySelector('.header__texto h2'); // Retorna ya sea ninguno o hasta un elemento que concuerde con el selector que estamos escribiendo
+console.log(heading)
+
+// querySelectorAll
+
+
+
+// getElementById
