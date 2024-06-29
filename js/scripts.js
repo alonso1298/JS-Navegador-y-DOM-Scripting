15c604e8a661a00276280a91c5016145f8661dec
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2024-06-29 03:16:42 -0600

    Se agrega querySelector

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,8 @@
 
 // querySelector 
 const heading = document.querySelector('.header__texto h2'); // Retorna ya sea ninguno o hasta un elemento que concuerde con el selector que estamos escribiendo
+heading.textContent = 'Nuevo Heading'; //Cambia el contenido del texto
+heading.classList.add('nueva-clase'); //Agrega una nueva clase
 console.log(heading)
 
 // querySelectorAll
