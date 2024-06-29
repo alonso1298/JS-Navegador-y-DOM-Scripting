3dbc4ae689c2a04fca99a9efe929113450521cd8
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2024-06-29 03:51:03 -0600

    Se agrega createElement

--- js/scripts.js
+++ js/scripts.js
@@ -12,9 +12,27 @@ console.log(enlaces[0]); // Para seleccionar un elace en especifico se hace por 
 
 enlaces[0].textContent = 'Nuevo texto para enlace';
 enlaces[0].classList.add('clase-nueva');
-enlaces[0].classList.remove('navegacion__enlace'); //Sirve para eliminar clases
+// enlaces[0].classList.remove('navegacion__enlace'); //Sirve para eliminar clases
 // enlaces[0].href = 'https://google.com'; //Se puede cambiar el enlace a la que te redirige 
 
 // getElementById
 const heading2 = document.getElementById('heading'); // Retorna 0 hasta 1 elemento que concuerde con su ID
 console.log(heading2)
+
+
+// Generar un nuevo enlace 
+const nuevoEnlace = document.createElement('A'); //createElement nos permite crear codigo HTML
+
+//Agregar el href
+nuevoEnlace.href = 'nuevo-enlace.html';
+//Agregar el texto 
+nuevoEnlace.textContent = 'Nuevo enlace';
+//Agregar la clase
+nuevoEnlace.classList.add('navegacion__enlace');
+
+//Agregarlo al documento
+const navegacion = document.querySelector('.navegacion');
+navegacion.appendChild(nuevoEnlace);
+
+console.log(nuevoEnlace)
+
